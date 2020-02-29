b3359f6ca1c8c492054173d4bdfeaf9e714b39b6
GazApp <juangaz@hotmail.com>
2020-02-28 21:59:04 -0600

    SE CONCLUYEN CAMPOS DENTROS DE LAS CLASES

---
+++ HumanResources/src/main/java/mx/com/hittechs/model/Employee.java
@@ -0,0 +1,29 @@
+
+package mx.com.hittechs.model;
+
+
+public class Employee {
+
+ public int id;  
+ public String firstName;
+ public String lastName;
+ public int age;
+ public String email;
+ public String phoneNumber;
+ public double salary;
+ public String hireDate; 
+ 
+   public void getDetails(){
+        
+        System.out.println("Id: " + id);
+        System.out.println("FirstName: " + firstName);
+        System.out.println("LastName: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("PhoneNumber: " + phoneNumber);
+        System.out.println("HireDate: " + hireDate);
+        
+        
+    }
+    
+}

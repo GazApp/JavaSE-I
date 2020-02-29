a0b003269758a03cdf694a32f223833c047fee9f
GazApp <juangaz@hotmail.com>
2020-02-28 21:59:10 -0600

    SE CONCLUYEN CAMPOS DENTROS DE LAS CLASES

--- HumanResources/src/main/java/mx/com/hittechs/test/HumanResourcesTest.java
+++ HumanResources/src/main/java/mx/com/hittechs/test/HumanResourcesTest.java
@@ -1,5 +1,7 @@
 package mx.com.hittechs.test;
 
+import mx.com.hittechs.model.Employee;
+
 /**
  *
  * @author Hittechs Training & Consulting
@@ -7,5 +9,32 @@ package mx.com.hittechs.test;
  * @mail contacto@hittechs.com.mx
  */
 public class HumanResourcesTest {
-    
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+
+        e1.id = 1;
+        e1.firstName = "Edgar";
+        e1.lastName="Gomez";
+        e1.age = 27;
+        e1.email = "e.gomez@hittechs.com.mx";
+        e1.phoneNumber = "123.456.7890";
+        e1.salary = 4500.25;
+        e1.hireDate = "11-02-2017";
+
+        e2.id = 2;
+        e2.firstName = "Marya";
+        e2.lastName ="Estrada";
+        e2.age = 19;
+        e2.email = "m.estrada@hittechs.com.mx";
+        e2.phoneNumber = "987.456.1230";
+        e2.salary = 2800.33;
+        e2.hireDate = "13-04-2017";
+        
+        e1.getDetails();
+        System.out.println("------------------------------");
+        e2.getDetails();
+    }
+
 }
